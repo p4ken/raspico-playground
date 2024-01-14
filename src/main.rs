361ca4a3b859500ea0a368374f0c5ab02bb8be57
p4ken <commit@p4ken.skr.jp>
2024-01-14 16:05:25 +0900

    fix runtime error

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
 
@@ -16,13 +13,9 @@ mod pico;
 #[rp2040_hal::entry]
 fn main() -> ! {
     info!("Program start");
+
     let mut pico = PicoW::new();
 
-    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
-    // on-board LED, it might need to be changed.
-    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead. If you have
-    // a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
-    // LED to one of the GPIO pins, and reference that pin here.
     let mut led_pin = pico.pins.gpio16.into_push_pull_output();
 
     loop {
--- src/pico.rs
+++ src/pico.rs
@@ -1,3 +1,11 @@
+/// The linker will place this boot block at the start of our program image. We
+/// need this to help the ROM bootloader get our code up and running.
+/// Note: This boot block is not necessary when using a rp-hal based BSP
+/// as the BSPs already perform this step.
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
 /// External high-speed crystal on the Raspberry Pi Pico board is 12 MHz. Adjust
 /// if your board has a different frequency
 const XTAL_FREQ_HZ: u32 = 12_000_000u32;
@@ -14,8 +22,8 @@ impl PicoW {
 }
 
 struct Rp2040 {
-    pub pins: rp2040_hal::gpio::Pins,
-    pub timer: rp2040_hal::Timer,
+    pins: rp2040_hal::gpio::Pins,
+    timer: rp2040_hal::Timer,
 }
 impl Rp2040 {
     pub fn new() -> Self {
