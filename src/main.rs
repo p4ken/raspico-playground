5284709c29808e2b31727c6cf8037e3d6932ac69
p4ken <commit@p4ken.skr.jp>
2024-01-20 19:00:12 +0900

    unwrap -> ?

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use core::convert::Infallible;
+
 use defmt::info;
 use defmt_rtt as _;
 use embedded_hal::{
@@ -17,24 +19,25 @@ mod pico;
 fn main() -> ! {
     info!("Program start");
     let pico = PicoW::new();
-    blink(pico);
+    blink(pico).unwrap();
+    loop {}
 }
 
-fn blink(mut pico: PicoW) -> ! {
+fn blink(mut pico: PicoW) -> Result<(), Infallible> {
     let button = pico.pins.gpio15.into_pull_up_input();
     let mut led_green = pico.pins.gpio16.into_push_pull_output();
     let mut led_red = pico.pins.gpio17.into_push_pull_output();
 
     loop {
-        led_green.set_high().unwrap();
+        led_green.set_high()?;
         pico.timer.delay_ms(500);
-        if button.is_low().unwrap() {
-            led_red.set_high().unwrap();
+        if button.is_low()? {
+            led_red.set_high()?;
         }
         pico.timer.delay_ms(500);
-        led_green.set_low().unwrap();
+        led_green.set_low()?;
         pico.timer.delay_ms(500);
-        led_red.set_low().unwrap();
+        led_red.set_low()?;
         pico.timer.delay_ms(500);
     }
 }
