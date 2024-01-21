4252d441b29f000f3f1ff055c2d62c7a98017ac8
p4ken <commit@p4ken.skr.jp>
2024-01-21 18:46:04 +0900

    gradation

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use core::convert::Infallible;
 use defmt::info;
 use defmt_rtt as _;
 use embedded_hal::{
-    blocking::delay::DelayMs,
+    blocking::delay::{DelayMs, DelayUs},
     digital::v2::{InputPin, OutputPin},
     PwmPin,
 };
@@ -26,22 +26,32 @@ fn main() -> ! {
 
 fn blink(mut pico: PicoW) -> Result<(), Infallible> {
     let button = pico.pins.gpio15.into_pull_up_input();
-    let mut led_green = pico.pins.gpio16.into_push_pull_output();
-    // let mut led_red = pico.pins.gpio17.into_push_pull_output();
+    let mut led_green = pico.pwms.pwm0.channel_a;
+    led_green.output_to(pico.pins.gpio16);
 
     let mut led_red = pico.pwms.pwm1.channel_a;
     led_red.output_to(pico.pins.gpio18);
 
     loop {
-        led_green.set_high()?;
-        pico.timer.delay_ms(500);
-        // if button.is_low()? {
-        led_red.set_duty(10000);
-        // }
-        pico.timer.delay_ms(500);
-        led_green.set_low()?;
-        pico.timer.delay_ms(500);
-        led_red.set_duty(0);
-        pico.timer.delay_ms(500);
+        const PHASE_USEC: i32 = 5_000_000;
+        const MAX_GREEN_DUTY: u16 = u16::MAX;
+        const MAX_RED_DUTY: u16 = 10000;
+
+        for i in 0..MAX_GREEN_DUTY {
+            led_green.set_duty(i);
+            pico.timer.delay_us(PHASE_USEC / MAX_GREEN_DUTY as i32);
+        }
+        for i in 0..MAX_RED_DUTY {
+            led_red.set_duty(i);
+            pico.timer.delay_us(PHASE_USEC / MAX_RED_DUTY as i32);
+        }
+        for i in (0..MAX_GREEN_DUTY).rev() {
+            led_green.set_duty(i);
+            pico.timer.delay_us(PHASE_USEC / MAX_GREEN_DUTY as i32);
+        }
+        for i in (0..MAX_RED_DUTY).rev() {
+            led_red.set_duty(i);
+            pico.timer.delay_us(PHASE_USEC / MAX_RED_DUTY as i32);
+        }
     }
 }
