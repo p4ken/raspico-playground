3c63aa075079cc0f1f50386c0392dc4f9012bca4
p4ken <commit@p4ken.skr.jp>
2026-05-06 08:58:24 +0900

    Drive onboard LED

--- examples/pwm.rs
+++ examples/pwm.rs
@@ -21,15 +21,15 @@ fn main() -> ! {
 fn blink(mut pico: Pico) -> Result<(), Infallible> {
     let _button = pico.pins.gpio16.into_pull_up_input();
 
-    let mut led_green = pico.pwms.pwm7.channel_b;
-    led_green.output_to(pico.pins.gpio15);
+    let mut led_green = pico.pwms.pwm4.channel_b;
+    led_green.output_to(pico.pins.led);
 
     let mut led_red = pico.pwms.pwm7.channel_a;
     led_red.output_to(pico.pins.gpio14);
 
     loop {
-        const PHASE_USEC: u32 = 5_000_000;
-        const MAX_GREEN_DUTY: u16 = u16::MAX;
+        const PHASE_USEC: u32 = 1_000_000;
+        const MAX_GREEN_DUTY: u16 = 1000;
         const MAX_RED_DUTY: u16 = 10000;
 
         for i in 0..MAX_GREEN_DUTY {
